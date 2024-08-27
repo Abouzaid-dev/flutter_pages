8392acbe1c40c8a237f7b98059e5858735915801
Mahmoud Abouzaid <m.abouzaid.flutter.dev@gmail.com>
2024-08-27 03:12:41 +0300

    file chnged

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:project/onboarding_screens/onboarding10/onboarding10.dart';
-=======
+import 'package:project/onboarding_screens/onboarding10.dart';
+
 import 'package:project/onboarding_screens/onboarding1.dart';
 
 
@@ -16,9 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
 
-      home: OnboardingScreen(),
-=======
-      home: Onboarding1(),
+
+
 
     );
   }
