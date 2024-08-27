a4c7d8fb573a0b34daaf358d8b5d5341e8633321
Normal-user69 <hm3446092@gmail.com>
2024-08-27 18:12:42 +0300

    Splash 7 Final Commit
    -Made The Login Text instead of pic
    -fixed some broken structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:project/splash_screens/splash_screen_7.dart';
 
-void main(){
-  runApp(const SplashScreen7());
+import 'package:project/onboarding_screens/onboarding10.dart';
+
+import 'package:project/onboarding_screens/onboarding1.dart';
+import 'package:project/splash_screens/splash_screen7.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+
+    );
+  }
 }
