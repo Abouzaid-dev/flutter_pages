b78f5b81064737c731def573f679dcd067b76a06
Nada Adel <nadaa43437@gmail.com>
2024-08-30 14:28:44 +0300

    status2

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:project/status_screens/status2.dart';
 
-import 'package:project/splash_screens/splash17.dart';
-
-import 'package:project/onboarding_screens/onboarding_8.dart';
-
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
-    );
-  }
+      home: StatusScreen2(),
+      );
 }
+  }
